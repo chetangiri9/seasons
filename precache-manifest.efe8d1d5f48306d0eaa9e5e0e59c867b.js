7add7490a1d5d7af484323028ed72660883ac6c2
“chetangiri9” <chetangiri9@gmail.com>
2019-08-25 12:22:41 +0530

    Updates

---
+++ precache-manifest.efe8d1d5f48306d0eaa9e5e0e59c867b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7238ea4dbf0bafac3058028204e7f9c",
+    "url": "/seasons/index.html"
+  },
+  {
+    "revision": "f657448b0496371ece4a",
+    "url": "/seasons/static/css/main.c5c5e061.chunk.css"
+  },
+  {
+    "revision": "607320d00d64b68e18da",
+    "url": "/seasons/static/js/2.2050718e.chunk.js"
+  },
+  {
+    "revision": "f657448b0496371ece4a",
+    "url": "/seasons/static/js/main.2fca08f0.chunk.js"
+  },
+  {
+    "revision": "808fed5ac1e88b432622",
+    "url": "/seasons/static/js/runtime~main.fbc5b39b.js"
+  }
+]);
